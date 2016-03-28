6ee2a08e4e089f6b0b843eed172658ba450b77e4
kate hall and Ned Faulhaber <ned@>
2016-03-28 14:07:19 -0700

    add reordering of userfavs array

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,16 @@ $(document).ready(function() {
       var fruit = ($("#inputFavoriteFruit").val()).toLowerCase();
       var film = ($("#inputFavoriteFilm").val()).toLowerCase();
       var food = ($("#inputFavoriteFood").val()).toLowerCase();
-      var userfavs = [fruit + ", " + film + ", " + food];
+      var userfavs = [fruit, film, food];
+      var newarray = userfavs.slice();
+      newarray.pop();
+      newarray.pop();
+      newarray.pop();
+      newarray.push(film, fruit, food);
 
-$(".fruitresult").text(userfavs);
+
+$("#result").text(newarray);
+
 
   event.preventDefault();
   });
